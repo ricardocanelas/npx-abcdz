59d625cb58bf65b7c1f8d12dd99ec0301cc7a6f1
Ricardo Canelas <ricardocanelas@gmail.com>
2019-10-22 11:37:47 -0300

    feat: update

--- packages/cli/cli.js
+++ packages/cli/cli.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
 console.log("is it working?")
+
+const files = [
+  { filename: './../../dist/main.js', output: 'main.js' },
+]
+
+const content = fs.readFileSync(files[0].filename)
+const output = './' + files[0].output
+
+fs.writeFileSync(output, content.toString())
