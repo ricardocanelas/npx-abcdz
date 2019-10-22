1cc1b94efa16b778b50be65b9d9449952a8dd651
Ricardo Canelas <ricardocanelas@gmail.com>
2019-10-22 12:31:10 -0300

    feat: update

--- packages/cli/cli.js
+++ packages/cli/cli.js
@@ -6,18 +6,18 @@ const fs = require('fs-extra')
 console.log("is it working?")
 
 const indexjs = path.join(__dirname, 'index.js');
-const mainjs = path.join(__dirname, '..', '..', 'dist', 'main.js');
+// const mainjs = path.join(__dirname, '..', '..', 'dist', 'main.js');
 
-const ownPath = path.dirname(
-  require.resolve(path.join(__dirname, '..', '..', 'dist', 'main.js'))
-);
+// const ownPath = path.dirname(
+//   require.resolve(path.join(__dirname, '..', '..', 'dist', 'main.js'))
+// );
 
 console.log('index.js', indexjs);
-console.log('main.js', mainjs);
+// console.log('main.js', mainjs);
 console.log('ownPath', ownPath);
 
 const contentIndex = fs.readFileSync(indexjs)
 console.log(contentIndex.toString());
 
-const contentMain = fs.readFileSync(mainjs)
-console.log(contentMain.toString());
+// const contentMain = fs.readFileSync(mainjs)
+// console.log(contentMain.toString());
